da0a90bf908178463c360881bd6a36a16aef71da
Giray Yaglikci <giray.yaglikci@safari.ethz.ch>
2025-08-27 21:09:44 +0200

    Join button;

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -31,10 +31,25 @@ module.exports = {
     <a href="https://www.github.com/agyaglikci"><i class="fa-brands fa-github fact-icon"></i></a>
     <a href="https://www.youtube.com/channel/UCPqbz23zyVRXRFv-vtNyZGw"><i class="fa-brands fa-youtube fact-icon"></i></a>
     <a href="https://www.linkedin.com/in/agyaglikci/"><i class="fa-brands fa-linkedin fact-icon"></i></a>
-    <a href="https://twitter.com/agyaglikci"><i class="fa-brands fa-twitter fact-icon"></i></a>`,
+    <a href="https://twitter.com/agyaglikci"><i class="fa-brands fa-twitter fact-icon"></i></a>
+    <a href="join.html" style="display:inline-block;
+            padding:6px 14px;
+            border:1px solid #2a7ae2;
+            border-radius:6px;
+            text-decoration:none;
+            color:#2a7ae2;
+            font-weight:500;">
+    Work With Me
+  </a>`,
     'email': '<a href="mailto:giray.yaglikci@safari.ethz.ch"><i class="fa fa-envelope fact-icon"></i>giray.yaglikci@safari.ethz.ch</a>',
     'safari': '<a href="https://safari.ethz.ch"><i class="fa fa-up-right-from-square fact-icon"></i>https://safari.ethz.ch</a>',
-    'cispa': '<a href="https://cispa.de"><i class="fa fa-up-right-from-square fact-icon"></i>https://cispa.de</a>',
+    'cispa': '<a href="https://cispa.de"><i class="fa fa-up-right-from-square fact-icon"></i>https://cispa.de</a>'
+    // 'Phone': '<a href="tel:+1234567890"><i class="fa fa-phone fact-icon"></i>+1 (234) 567-890</a>',
+    // 'ORCID': '<a href="https://orcid.org/0000-0002-1825-0097"><i class="fa-brands fa-orcid fact-icon"></i>0000-0002-1825-0097</a>',
+    // 'ResearcherID': '<a href="http://www.researcherid.com/rid/A-1009-2009"><i class="fa fa-id-badge fact-icon"></i>A-1009-2009</a>',
+    // 'Scopus': '<a href="https://www.scopus.com/authid/detail.uri?authorId=7004212771"><i class="fa fa-search fact-icon"></i>7004212771</a>',
+    // 'GScholar': '<a href="https://scholar.google.com/citations?user=XXXXXX"><i class="fa fa-graduation-cap fact-icon"></i>XXXXXX</a>',
+    // 'ORCID': '<a href="https://orcid.org/0000-0002-1825-0097"><i class="fa-brands fa-orcid fact-icon"></i>0000-0002-1825-0097</a>',
     
   },
   skills: [
